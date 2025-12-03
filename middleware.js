7198a1428a27df9549a95b2d0b63a9920b17bd3a
Krishna Pratap Singh Chauhan <145362216+krishnapschauhan@users.noreply.github.com>
2025-12-04 00:38:48 +0530

    Enhance middleware with route protection logic

--- middleware.js
+++ middleware.js
@@ -1,6 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtected = createRouteMatcher([
+  "/dashboard(.*)",
+  "/journal(.*)",
+  "/collection(.*)"
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!auth().userId && isProtected(req)) {
+    return auth().redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
