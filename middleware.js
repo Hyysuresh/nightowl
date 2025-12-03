41f9b58979669c7468e63061ac35e3c1f143acf9
Krishna Pratap Singh Chauhan <145362216+krishnapschauhan@users.noreply.github.com>
2025-12-04 00:47:47 +0530

    Refactor middleware to remove route protection
    Removed route protection logic and simplified middleware export.

--- middleware.js
+++ middleware.js
@@ -1,24 +1,10 @@
-//clerk path auth
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/journal(.*)",
-  "/collection(.*)",
-]);
-
-export default clerkMiddleware((auth, req) => {
-  const { userId } = auth();
-
-  // Block only if NOT logged in AND trying to access protected routes
-  if (!userId && isProtectedRoute(req)) {
-    return auth().redirectToSignIn({ returnBackUrl: req.url });
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
     "/((?!_next|.*\\..+).*)",
-    "/(api|trpc)(.*)",
+    "/(api|trpc)(.*)"
   ],
 };
